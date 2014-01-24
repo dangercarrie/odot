45d1eb5e26a419ed942d23771b409fe8452cf5a2
Danger Carrie <dangercarrie@gmail.com>
2014-01-23 22:42:44 -0600

    Added validations to new todo items

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -20,4 +20,26 @@ describe "Adding todo items" do
 			expect(page).to have_content("Milk")
 		end
 	end
+
+	it "should display an error when there is no content" do
+		visit_todo_list(todo_list)
+		click_link "New Todo Item"
+		fill_in "Content", with: ""
+		click_button "Save"
+		within("div.flash") do
+			expect(page).to have_content("There was an error adding that todo list item.")
+		end
+			expect(page).to have_content("Content can't be blank")
+	end
+
+	it "should display an error when there is no content" do
+		visit_todo_list(todo_list)
+		click_link "New Todo Item"
+		fill_in "Content", with: ""
+		click_button "Save"
+		within("div.flash") do
+			expect(page).to have_content("There was an error adding that todo list item.")
+		end
+			expect(page).to have_content("Content is too short")
+		end
 end
